8a6ed4801464a3ba80b6d8c3d5b046c90fd0e08b
emile <emile@globenet.org>
2025-05-25 22:04:47 +0200

    correct out build path

--- build_onlyoffice.sh
+++ build_onlyoffice.sh
@@ -2,4 +2,4 @@
 
 . .env
 
-docker run -e PRODUCT_VERSION="${PRODUCT_VERSION}" -e BUILD_NUMBER="${BUILD_NUMBER}" -v $(pwd)/out:/build_tools/out onlyoffice-document-editors-builder-deps
+docker run -e PRODUCT_VERSION="${PRODUCT_VERSION}" -e BUILD_NUMBER="${BUILD_NUMBER}" -v $(pwd)/../build_tools/out:/build_tools/out onlyoffice-document-editors-builder-deps
