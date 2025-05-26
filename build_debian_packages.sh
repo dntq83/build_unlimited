81ad99c54fe0b528c2fa49f27902f8c905b72c05
emile <emile@globenet.org>
2025-05-26 08:44:29 +0200

    build_debian_packages.sh typo

--- build_debian_packages.sh
+++ build_debian_packages.sh
@@ -2,4 +2,4 @@
 
 . .env
 
-docker run -v $(pwd)/../build_tools:/build_tools:ro -v $(pwd)/../document-server-package:/document-server-package:rw onlyoffice-deb-builder /bin/bash
+docker run -v $(pwd)/../build_tools:/build_tools:ro -v $(pwd)/../document-server-package:/document-server-package:rw onlyoffice-deb-builder
