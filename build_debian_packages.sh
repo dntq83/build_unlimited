ba669d7a0713f7acbe3543c9e755e9aeb95df517
DNTQ Vu <5275039+dntq83@users.noreply.github.com>
2026-02-25 10:55:56 +0700

    Update build_debian_packages.sh

--- build_debian_packages.sh
+++ build_debian_packages.sh
@@ -2,4 +2,4 @@
 
 . .env
 
-docker run -v $(pwd)/../build_tools:/build_tools:ro -v $(pwd)/../document-server-package:/document-server-package:rw  -v $(pwd)/newfonts:/newfonts:ro onlyoffice-deb-builder
+docker run -v $(pwd)/../build_tools:/build_tools:ro -v $(pwd)/../document-server-package:/document-server-package:rw  onlyoffice-deb-builder
